143c3e4048d6e40c90ad5758e3150eb23340e5d9
Gustavo Tino <gustavotino8@gmail.com>
2021-02-03 19:27:07 -0300

    finish lookat

--- Math 4 Gamedev/Part 1/Look-at Trigger/trigger.gd
+++ Math 4 Gamedev/Part 1/Look-at Trigger/trigger.gd
@@ -2,22 +2,28 @@ extends Node2D
 
 export(Color) var normal_color := Color(1, 1, 1, 1)
 export(Color) var trigger_color := Color(1, 0, 0, 1)
-export(float) var tol := 0.5
+export(float) var tol := 0.98
 export(int) var radius := 200
 
 var from := Vector2(0, 0)
 var to := Vector2(0, 0)
 
+var cos_: float
+var triggered: bool
 var point: Node
+var point_color: Color
 var point_pos: Vector2
 var norm_pos: Vector2
-var diff: Vector2
+var arc_pos: Vector2
+var arc_pos_point: Vector2
+var norm_pos_point: Vector2
 var line_color := normal_color
 
 
 func _ready():
 	point = get_node("../point")
 	point_pos = point.pos
+	point_color = point.color*0.2
 
 func _process(delta):
 	point_pos = point.pos
@@ -28,8 +34,21 @@ func _process(delta):
 	update()
 
 func _draw():
-	diff = to - from
-	norm_pos = diff.normalized()*radius
+	norm_pos = (to - from).normalized()
+	norm_pos_point = (point_pos - from).normalized()
 	
-	draw_line(from, from + norm_pos, line_color, 2.0, true)
+	arc_pos = norm_pos*radius
+	arc_pos_point = norm_pos_point*radius
+	
+	#0.9 - 0.5 = 0.4
+	
+	cos_ = norm_pos.dot(norm_pos_point)
+	if cos_ >= 0.5 and (tol - cos_) <= 0:
+		line_color = trigger_color
+	else:
+		line_color = normal_color
+	
+	draw_line(from, from + arc_pos_point, point_color, 2.0, true)
+	
+	draw_line(from, from + arc_pos, line_color, 2.0, true)
 	draw_circle(from, radius, Color(0, 0, 0, 0.1))
