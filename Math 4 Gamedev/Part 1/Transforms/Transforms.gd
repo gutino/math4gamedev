93a2ab8b866c55312ddd3d6c8f9e64c60d2186d2
Gustavo Tino <gustavotino8@gmail.com>
2021-02-04 19:18:33 -0300

    fix orientation error

--- Math 4 Gamedev/Part 1/Transforms/Transforms.gd
+++ Math 4 Gamedev/Part 1/Transforms/Transforms.gd
@@ -1,4 +1,4 @@
-#tool
+tool
 extends Node2D
 
 var scale_fac := 100
@@ -9,11 +9,14 @@ var local: Vector2
 var obj_origin: Vector2
 var point_origin: Vector2
 
-onready var object = get_node("object")
-onready var point = get_node("point")
+var object: Node2D
+var point: Node2D
 
 
 func _process(delta):
+	object = get_node("object")
+	point = get_node("point")
+	
 	obj_origin = object.transform.origin
 	point_origin = point.transform.origin
 
@@ -21,9 +24,19 @@ func _process(delta):
 	obj_y = object.transform.orthonormalized().y
 	
 	local = Vector2(
-		obj_x.dot(point.transform.origin), 
-		obj_y.dot(point.transform.origin)
+		obj_y.dot(point.transform.origin), 
+		obj_x.dot(point.transform.origin)
 	)
+	
+	print('---')
+	print(object.global_position)
+	print(object.global_transform)
+	print(point.global_position)
+	print(point.global_transform)
+	#print(obj_x)
+	#print(obj_y)
+	#print(local)
+	#print(object.transform.basis_xform(point_origin))
 	
 	update()
 
@@ -36,4 +49,5 @@ func _draw():
 	draw_line(obj_origin, (obj_origin + obj_y*scale_fac), Color(0,1,0,1))
 	
 	draw_line(obj_origin, obj_origin + local, Color(1,1,1,1), 3.0)
+	
 	draw_circle(obj_origin, 5.0, Color(0,0,0,1))
